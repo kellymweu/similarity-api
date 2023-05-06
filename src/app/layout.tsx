51745f801d483d68689b963fd011b5753771a57c
Kelly Mweu <mweukelly@gmail.com>
2023-05-06 21:23:53 +0300

    Update layout.tsx
    The navbar was commented out at first.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/Toast";
+import Navbar from "@/components/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -17,12 +19,14 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
-          {children}
-
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
           <Toaster position="bottom-right" />
-          {/*<Navbar /> */}
+
+          <main>{children}</main>
         </Providers>
-        {/* Allow for more height on mobile devices */}
+
+        {/* Allow more height for mobile menu on mobile */}
         <div className="h-40 md:hidden" />
       </body>
     </html>
